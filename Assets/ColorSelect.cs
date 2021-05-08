9b517675cca3de0422ec446cad7bc3fb84e6b4af
tejaskachare <tejas.kachare@gmail.com>
2021-05-08 20:23:40 +0530

    initial commit

---
+++ Assets/ColorSelect.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ColorSelect : MonoBehaviour
+{
+    public Material band1;
+    public Material band2;
+    public Material band3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public void w1ColorSelect()
+    {
+        string buttonName = EventSystem.current.currentSelectedGameObject.name;
+        if (buttonName == "Black button")
+        {
+            //changing the color to black
+            Color myColor = new Color();
+            ColorUtility.TryParseHtmlString("#000000", out myColor);
+            band1.color = myColor;
+        }
+        else if (buttonName == "Blue button")
+        {
+            //changing the color to blue
+            Color myColor = new Color();
+            ColorUtility.TryParseHtmlString("#0724B3", out myColor);
+            band1.color = myColor;
+        }
+    }
+    public void w2ColorSelect()
+    {
+        string buttonName = EventSystem.current.currentSelectedGameObject.name;
+        if (buttonName == "Black button")
+        {
+            //changing the color to black
+            Color myColor = new Color();
+            ColorUtility.TryParseHtmlString("#000000", out myColor);
+            band2.color = myColor;
+        }
+        else if (buttonName == "Red button")
+        {
+            //changing the color to red
+            Color myColor = new Color();
+            ColorUtility.TryParseHtmlString("#740202", out myColor);
+            band2.color = myColor;
+        }
+    }
+    public void w3ColorSelect()
+    {
+        string buttonName = EventSystem.current.currentSelectedGameObject.name;
+        if (buttonName == "Black button")
+        {
+            //changing the color to black
+            Color myColor = new Color();
+            ColorUtility.TryParseHtmlString("#464646", out myColor);
+            band3.color = myColor;
+        }
+        else if (buttonName == "Silver button")
+        {
+            //changing the color to silver
+            Color myColor = new Color();
+            ColorUtility.TryParseHtmlString("#A1A1A1", out myColor);
+            band3.color = myColor;
+        }
+    }
+}
